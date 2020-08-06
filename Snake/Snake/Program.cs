6d5e3b4ff54feb902ca2b6c92892c35ec2d11044
Rev1ved <Rev1ved911@gmail.com>
2020-08-06 14:12:45 +0700

    obstacles

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,31 +9,66 @@ namespace Snake
 {
     class Program
     {
-        static void Draw(Figure figure)
-        {
-            figure.Draw();
-        }
         static void Main(string[] args)
         {
-            VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
-            Draw(v1);
+            Console.SetBufferSize(80, 25);
 
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+            // Отрисовка точек
             Point p = new Point(4, 5, '*');
-            Figure fSnake = new Snake(p, 4, Direction.RIGHT);
-            Draw(fSnake);
-            Snake snake = (Snake)fSnake;
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
 
-            HorisontalLine h1 = new HorisontalLine(0, 5, 6, '&');
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
 
-            List<Figure> figures = new List<Figure>();
-            figures.Add(fSnake);
-            figures.Add(v1);
-            figures.Add(h1);
-
-            foreach(var f in figures)
+            while(true)
             {
-                f.Draw();
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
             }
+
+
+            //VerticalLine v1 = new VerticalLine(0, 10, 5, '%');
+            //Draw(v1);
+
+            //Point p = new Point(4, 5, '*');
+            //Figure fSnake = new Snake(p, 4, Direction.RIGHT);
+            //Draw(fSnake);
+            //Snake snake = (Snake)fSnake;
+
+            //HorisontalLine h1 = new HorisontalLine(0, 5, 6, '&');
+
+            //List<Figure> figures = new List<Figure>();
+            //figures.Add(fSnake);
+            //figures.Add(v1);
+            //figures.Add(h1);
+
+            //foreach(var f in figures)
+            //{
+            //    f.Draw();
+            //}
 
             //Console.SetBufferSize(80, 25);
 
